0b3b98e2940217e7a71d7535309d4c69cdb79280
Terra Manning <terradmanning@gmail.com>
2025-01-20 17:15:33 -0700

    feat: controller logic and model set up

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  has_many :users_shows, dependent: :destroy
+  has_many :shows, through: :users_shows
+
+  validates :username, :first_name, :last_name, presence: true
 end
