beafdb29b7a2532e1d6596e8273cd1a913b8f4ac
Terra Manning <terradmanning@gmail.com>
2025-01-20 18:00:00 -0700

    feat: set up model tests with associations and validations

--- spec/models/show_spec.rb
+++ spec/models/show_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Show, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it { should have_many(:users_shows).dependent(:destroy) }
+    it { should have_many(:users).through(:users_shows) }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of(:artist) }
+    it { should validate_presence_of(:location) }
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it { should have_many(:users_shows).dependent(:destroy) }
+    it { should have_many(:shows).through(:users_shows) }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of(:username) }
+    it { should validate_presence_of(:first_name) }
+    it { should validate_presence_of(:last_name) }
+  end
 end
