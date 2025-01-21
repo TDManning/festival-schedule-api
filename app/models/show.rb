a02baacd980524ced1481d2d52bd55b728e6a629
Terra Manning <terradmanning@gmail.com>
2025-01-20 17:22:07 -0700

    fix: minor adjustments to models

--- app/models/show.rb
+++ app/models/show.rb
@@ -2,6 +2,5 @@ class Show < ApplicationRecord
   has_many :users_shows, dependent: :destroy
   has_many :users, through: :users_shows
 
-  validates :artist, :location, :time_slot, presence: true
-  validates :time_slot, numericality: { only_integer: true }
+  validates :artist, :location, presence: true
 end
--- app/models/users_show.rb
+++ app/models/users_show.rb
@@ -1,14 +1,4 @@
 class UsersShow < ApplicationRecord
   belongs_to :user
   belongs_to :show
-
-  validate :unique_user_show_combination
-
-  private
-
-  def unique_user_show_combination
-    if UsersShow.exists?(user_id: user_id, show_id: show_id)
-      errors.add(:base, 'Show already added to schedule')
-    end
-  end
 end
