4510c1737316e24b8d419890b0a347954327619f
Terra Manning <terradmanning@gmail.com>
2025-01-21 21:06:15 -0700

    feat: add a method for favorited in show serializer

--- app/serializers/show_serializer.rb
+++ app/serializers/show_serializer.rb
@@ -1,5 +1,9 @@
 class ShowSerializer
   include JSONAPI::Serializer
 
-  attributes :id, :artist, :location, :time_slot, :image_url
+  attributes :id, :artist, :location, :time_slot, :image_url, :favorited
+
+  attribute :favorited do |show, params|
+    params[:user]&.favorites&.exists?(show.id) || false
+  end
 end
