dcc1392922d0e06a543f7ae061ac087983e2b7cb
Terra Manning <terradmanning@gmail.com>
2025-02-08 16:54:09 -0700

    Disable master key requirement for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,6 +80,9 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.require_master_key = false
+
+
   # Enable DNS rebinding protection and other `Host` header attacks.
   # config.hosts = [
   #   "example.com",     # Allow requests from example.com
